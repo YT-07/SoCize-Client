b482a4dd8442f7a0cf888d389bf22f39a29f7d88
YT-07 <no-email>
2025-05-23 18:04:01 +0800

    Added logging for FileIO utility method

--- src/main/java/com/socize/utilities/FileIO.java
+++ src/main/java/com/socize/utilities/FileIO.java
@@ -5,11 +5,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Stack;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Class to provide common method for file I/O operations.
  */
 public class FileIO {
-    
+    private static final Logger logger = LoggerFactory.getLogger(FileIO.class);
+
     private FileIO() {
         throw new UnsupportedOperationException("Utility class not meant to be instantiated.");
     }
@@ -33,16 +37,18 @@ public class FileIO {
                     Files.deleteIfExists(filePath);
 
                 } catch (Exception e) {
-                    // TODO: Log error if fail to delete file
+                    logger.error("Failed to delete file '{}'.", filePath.toString(), e);
                 }
             }
             
         };
 
         Files.createFile(filePath); // If exception occur here, will not register rollback function to stack
+        logger.info("File '{}' successfully created.", filePath.toString());
         
         if(rollbackStack != null) {
             rollbackStack.push(rollbackCreateFileTask);
+            logger.info("Successfully registered rollback task to delete file '{}'.", filePath.toString());
         }
     }
 }
