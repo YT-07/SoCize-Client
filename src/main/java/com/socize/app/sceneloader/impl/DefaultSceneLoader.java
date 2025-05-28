04963ad27f2dbe726cc6469e897000ca6f117784
YT-07 <no-email>
2025-05-28 21:35:20 +0800

    Refactored scene loader to use scene factory for scene loading to improve testability

--- src/main/java/com/socize/app/sceneloader/impl/DefaultSceneLoader.java
+++ src/main/java/com/socize/app/sceneloader/impl/DefaultSceneLoader.java
@@ -1,32 +1,27 @@
 package com.socize.app.sceneloader.impl;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.HashMap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.socize.app.sceneloader.AppScene;
-import com.socize.app.sceneloader.spi.SceneControllerFactory;
+import com.socize.app.sceneloader.spi.SceneFactory;
 import com.socize.app.sceneloader.spi.SceneLoader;
 
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
 public class DefaultSceneLoader implements SceneLoader {
     private static DefaultSceneLoader instance;
-    private static final Logger logger = LoggerFactory.getLogger(DefaultSceneLoader.class);
 
-    private HashMap<AppScene, Parent> scenes;
+    private final HashMap<AppScene, Parent> scenes;
+    private final SceneFactory sceneFactory;
 
-    private DefaultSceneLoader() {
+    private DefaultSceneLoader(SceneFactory sceneFactory) {
         scenes = new HashMap<>();
+        this.sceneFactory = sceneFactory;
     }
 
     public static DefaultSceneLoader getInstance() {
         if(instance == null) {
-            instance = new DefaultSceneLoader();
+            instance = new DefaultSceneLoader(new DefaultSceneFactory());
         }
 
         return instance;
@@ -36,24 +31,8 @@ public class DefaultSceneLoader implements SceneLoader {
     public Parent getScene(AppScene scene) {
         
         if(!scenes.containsKey(scene)) {
-            URL scenePath = getClass().getResource(scene.getPath());
-            FXMLLoader loader = new FXMLLoader(scenePath);
-
-            SceneControllerFactory controllerFactory = scene.getControllerFactory();
-            loader.setController(controllerFactory.createDefault());
-
-            try {
-
-                Parent parent = loader.load();
-                scenes.put(scene, parent);
-
-            } catch (IOException ioe) {
-                logger.error("Error loading scene '{}'.", scene.name(), ioe);
-
-                throw new RuntimeException();
-
-            }
-
+            Parent parent = sceneFactory.load(scene);
+            scenes.put(scene, parent);
         }
 
         return scenes.get(scene);
