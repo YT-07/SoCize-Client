c2ba44a7e1c69cc5319b4d6a39c7d7210bc42e46
YT-07 <no-email>
2025-05-29 22:04:31 +0800

    Added logic to switch page for home page buttons

--- src/main/java/com/socize/pages/homepage/HomeController.java
+++ src/main/java/com/socize/pages/homepage/HomeController.java
@@ -3,6 +3,9 @@ package com.socize.pages.homepage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.socize.app.sceneloader.AppScene;
+import com.socize.shared.spi.MainMenuPageState;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -15,9 +18,21 @@ public class HomeController implements Initializable {
     @FXML
     private Button signInButton;
 
+    private final MainMenuPageState mainMenuPageState;
+
+    public HomeController(MainMenuPageState mainMenuPageState) {
+        this.mainMenuPageState = mainMenuPageState;
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+        signInButton.setOnAction(e -> {
+            mainMenuPageState.setPage(AppScene.SIGN_IN_PAGE);
+        });
+
+        signUpButton.setOnAction(e -> {
+            mainMenuPageState.setPage(AppScene.SIGN_UP_PAGE);
+        });
     }
     
 }
