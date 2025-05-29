e0322359afe406f6033b740e2809120ae9ae54c6
YT-07 <no-email>
2025-05-29 22:12:41 +0800

    Added button logic to switch page for sign in page

--- src/main/java/com/socize/pages/signin/SignInController.java
+++ src/main/java/com/socize/pages/signin/SignInController.java
@@ -3,6 +3,9 @@ package com.socize.pages.signin;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.socize.app.sceneloader.AppScene;
+import com.socize.shared.spi.MainMenuPageState;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -35,10 +38,21 @@ public class SignInController implements Initializable {
     @FXML
     private Text signInErrorText;
 
+    private final MainMenuPageState mainMenuPageState;
+
+    public SignInController(MainMenuPageState mainMenuPageState) {
+        this.mainMenuPageState = mainMenuPageState;
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+        homeButton.setOnAction(e -> {
+            mainMenuPageState.setPage(AppScene.HOME_PAGE);
+        });
+
+        signUpButton.setOnAction(e -> {
+            mainMenuPageState.setPage(AppScene.SIGN_UP_PAGE);
+        });
     }
     
 }
