fbbaf3517d1d38d46acd44fa9bf9b5907734b806
YT-07 <no-email>
2025-05-29 20:23:09 +0800

    Added controller for home page

---
+++ src/main/java/com/socize/pages/homepage/HomeController.java
@@ -0,0 +1,23 @@
+package com.socize.pages.homepage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class HomeController implements Initializable {
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private Button signInButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+    
+}
