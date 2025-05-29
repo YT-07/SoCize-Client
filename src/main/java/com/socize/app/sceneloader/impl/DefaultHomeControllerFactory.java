7cb04ec1c79c553989aab40fc42e61cf49ccdb9f
YT-07 <no-email>
2025-05-29 22:04:59 +0800

    Modified home controller's factory to inject required dependency

--- src/main/java/com/socize/app/sceneloader/impl/DefaultHomeControllerFactory.java
+++ src/main/java/com/socize/app/sceneloader/impl/DefaultHomeControllerFactory.java
@@ -2,12 +2,16 @@ package com.socize.app.sceneloader.impl;
 
 import com.socize.app.sceneloader.spi.SceneControllerFactory;
 import com.socize.pages.homepage.HomeController;
+import com.socize.shared.impl.DefaultMainMenuPageState;
+import com.socize.shared.spi.MainMenuPageState;
 
 public class DefaultHomeControllerFactory implements SceneControllerFactory {
 
     @Override
     public Object createDefault() {
-        return new HomeController();
+        MainMenuPageState mainMenuPageState = DefaultMainMenuPageState.getInstance();
+
+        return new HomeController(mainMenuPageState);
     }
     
 }
