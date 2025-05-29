ed77f102777361254914f96d41e1cbd793b10be2
YT-07 <no-email>
2025-05-29 21:55:43 +0800

    Modified main mene controller's factory to inject default dependencies

--- src/main/java/com/socize/app/sceneloader/impl/DefaultMainMenuControllerFactory.java
+++ src/main/java/com/socize/app/sceneloader/impl/DefaultMainMenuControllerFactory.java
@@ -1,13 +1,20 @@
 package com.socize.app.sceneloader.impl;
 
+import com.socize.app.sceneloader.SceneLoaders;
 import com.socize.app.sceneloader.spi.SceneControllerFactory;
+import com.socize.app.sceneloader.spi.SceneLoader;
 import com.socize.pages.mainmenu.MainMenuController;
+import com.socize.shared.impl.DefaultMainMenuPageState;
+import com.socize.shared.spi.MainMenuPageState;
 
 public class DefaultMainMenuControllerFactory implements SceneControllerFactory {
 
     @Override
     public Object createDefault() {
-        return new MainMenuController();
+        MainMenuPageState pageState = DefaultMainMenuPageState.getInstance();
+        SceneLoader loader = SceneLoaders.getDefault();
+
+        return new MainMenuController(pageState, loader);
     }
     
 }
