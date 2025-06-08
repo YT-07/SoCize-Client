414a46c08877f210600ec5ccfa2696f09b5344a9
YT-07 <no-email>
2025-06-08 14:37:27 +0800

    Added more test for file size tracker

--- src/test/java/com/socize/utilities/FileSizeTrackerTest.java
+++ src/test/java/com/socize/utilities/FileSizeTrackerTest.java
@@ -1,6 +1,8 @@
 package com.socize.utilities;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +13,35 @@ public class FileSizeTrackerTest {
         assertThrows(IllegalArgumentException.class, () -> {
             new FileSizeTracker(-1);
         });
+    }
+
+    @Test
+    void shouldReachMinFileSize_IfIncrementValueIsEqualToOrMoreThanMinFileSize() {
+        FileSizeTracker tracker1 = new FileSizeTracker(100);
+        FileSizeTracker tracker2 = new FileSizeTracker(100);
+
+        tracker1.increment(100);
+        tracker2.increment(200);
+
+        assertAll(
+            () -> {
+                assertTrue(tracker1.hasReachedMinFileSize());
+            },
+
+            () -> {
+                assertTrue(tracker2.hasReachedMinFileSize());
+            }
+        );
+        
+    }
+
+    @Test
+    void shouldNotIntegerOverflow_IfIncrementMoreThanIntegerMaxValue() {
+        FileSizeTracker tracker = new FileSizeTracker(Long.MAX_VALUE);
+
+        tracker.increment(Long.MAX_VALUE);
+        tracker.increment(1);
+
+        assertTrue(tracker.hasReachedMinFileSize());
     }
 }
