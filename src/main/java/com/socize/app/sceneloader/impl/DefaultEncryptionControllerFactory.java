a56b0c3440144e0d51a2978c95e564f6da666c1d
YT-07 <no-email>
2025-06-08 18:36:05 +0800

    Modified controller factory to include necessary dependency

--- src/main/java/com/socize/app/sceneloader/impl/DefaultEncryptionControllerFactory.java
+++ src/main/java/com/socize/app/sceneloader/impl/DefaultEncryptionControllerFactory.java
@@ -1,13 +1,30 @@
 package com.socize.app.sceneloader.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.socize.app.sceneloader.spi.SceneControllerFactory;
+import com.socize.encryption.impl.DefaultEncryptionService;
+import com.socize.encryption.spi.EncryptionService;
 import com.socize.pages.encryption.EncryptionController;
 
 public class DefaultEncryptionControllerFactory implements SceneControllerFactory {
+    private static final Logger logger = LoggerFactory.getLogger(DefaultEncryptionControllerFactory.class);
 
     @Override
     public Object createDefault() {
-        return new EncryptionController();
+        EncryptionService encryptionService = null;
+
+        try {
+
+            encryptionService = new DefaultEncryptionService();
+
+        } catch (Exception e) {
+            logger.error("Error initializing default encryption service.", e);
+            throw new RuntimeException();
+        }
+
+        return new EncryptionController(encryptionService);
     }
     
 }
