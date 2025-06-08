bbeec5c501dc6fab36cf488ceca8e16fd56df1f1
YT-07 <no-email>
2025-06-08 16:32:12 +0800

    Added file selection feature to select file and folder

--- src/main/java/com/socize/pages/encryption/EncryptionController.java
+++ src/main/java/com/socize/pages/encryption/EncryptionController.java
@@ -1,5 +1,6 @@
 package com.socize.pages.encryption;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -7,6 +8,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 
 public class EncryptionController implements Initializable {
 
@@ -28,9 +32,46 @@ public class EncryptionController implements Initializable {
     @FXML
     private Label encryptFeedbackField;
 
+    private final FileChooser fileChooser;
+    private final DirectoryChooser directoryChooser;
+
+    private File folderToSave;
+    private File fileToEncrypt;
+
+    public EncryptionController() {
+        this.fileChooser = new FileChooser();
+        this.fileChooser.setTitle("Select file to encrypt");
+
+        this.directoryChooser = new DirectoryChooser();
+        this.directoryChooser.setTitle("Select folder to save");
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+        selectFileToEncryptButton.setOnAction(e -> selectFileToEncrypt());
+
+        selectFolderToSaveButton.setOnAction(e -> selectFolderToSave());
     }
     
+    /**
+     * Helper function to orchestrate the process of selecting 
+     * the file to encrypt.
+     */
+    private void selectFileToEncrypt() {
+        Window window = selectFileToEncryptButton.getScene().getWindow();
+        fileToEncrypt = fileChooser.showOpenDialog(window);
+
+        fileToEncryptPath.setText(fileToEncrypt.getAbsolutePath());
+    }
+
+    /**
+     * Helper function to orchestrate the process of selecting 
+     * the folder to save.
+     */
+    private void selectFolderToSave() {
+        Window window = selectFolderToSaveButton.getScene().getWindow();
+        folderToSave = directoryChooser.showDialog(window);
+
+        folderToSavePath.setText(folderToSave.getAbsolutePath());
+    }
 }
