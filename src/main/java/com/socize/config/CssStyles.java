1d7ee75f3fb00a67a21ad79154870e6d218ce85a
YT-07 <no-email>
2025-06-08 19:20:14 +0800

    Added config class to hold style name for success and error css style

---
+++ src/main/java/com/socize/config/CssStyles.java
@@ -0,0 +1,11 @@
+package com.socize.config;
+
+public class CssStyles {
+    
+    private CssStyles() {
+        throw new UnsupportedOperationException("Config file not meant to be instantiated.");
+    }
+
+    public static final String SUCCESS_STYLE = "successful-feedback-message";
+    public static final String ERROR_STYLE = "error-feedback-message";
+}
