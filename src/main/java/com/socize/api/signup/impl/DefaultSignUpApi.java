17ad1c3fdf60fb2be5b7ae544fd4c73582ce826f
YT-07 <no-email>
2025-06-09 20:12:14 +0800

    Moved sign up api into its own package

--- src/main/java/com/socize/api/signup/impl/DefaultSignUpApi.java
+++ src/main/java/com/socize/api/signup/impl/DefaultSignUpApi.java
@@ -1,8 +1,8 @@
-package com.socize.api.impl;
+package com.socize.api.signup.impl;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 
-import com.socize.api.spi.SignUpApi;
+import com.socize.api.signup.spi.SignUpApi;
 import com.socize.dto.SignUpRequest;
 
 public class DefaultSignUpApi implements SignUpApi {
