238a81b090c31b60c289b4aae66c6a38461872ef
YT-07 <no-email>
2025-06-09 20:23:13 +0800

    Added new dependency for sign in controller factory

--- src/main/java/com/socize/app/sceneloader/impl/DefaultSignInControllerFactory.java
+++ src/main/java/com/socize/app/sceneloader/impl/DefaultSignInControllerFactory.java
@@ -1,5 +1,7 @@
 package com.socize.app.sceneloader.impl;
 
+import com.socize.api.signin.impl.DefaultSignInApi;
+import com.socize.api.signin.spi.SignInApi;
 import com.socize.app.sceneloader.spi.SceneControllerFactory;
 import com.socize.pages.signin.SignInController;
 import com.socize.shared.mainmenupagestate.impl.DefaultMainMenuPageState;
@@ -10,8 +12,9 @@ public class DefaultSignInControllerFactory implements SceneControllerFactory {
     @Override
     public Object createDefault() {
         MainMenuPageState mainMenuPageState = DefaultMainMenuPageState.getInstance();
+        SignInApi signInApi = new DefaultSignInApi();
 
-        return new SignInController(mainMenuPageState);
+        return new SignInController(mainMenuPageState, signInApi);
     }
     
 }
