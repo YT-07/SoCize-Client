223f90902f9b9245855aaa82c0996b5c90725690
YT-07 <no-email>
2025-06-09 21:08:00 +0800

    Fixed controller factory to use new dependencies

--- src/main/java/com/socize/app/sceneloader/impl/DefaultSignInControllerFactory.java
+++ src/main/java/com/socize/app/sceneloader/impl/DefaultSignInControllerFactory.java
@@ -1,11 +1,15 @@
 package com.socize.app.sceneloader.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.socize.api.signin.impl.DefaultSignInApi;
 import com.socize.api.signin.spi.SignInApi;
 import com.socize.app.sceneloader.spi.SceneControllerFactory;
 import com.socize.pages.signin.SignInController;
+import com.socize.pages.signin.impl.DefaultSignInModel;
+import com.socize.pages.signin.spi.SignInModel;
 import com.socize.shared.mainmenupagestate.impl.DefaultMainMenuPageState;
 import com.socize.shared.mainmenupagestate.spi.MainMenuPageState;
+import com.socize.utilities.objectmapper.impl.DefaultObjectMapperProvider;
 import com.socize.utilities.textstyler.impl.DefaultTextStyler;
 import com.socize.utilities.textstyler.spi.TextStyler;
 
@@ -14,10 +18,14 @@ public class DefaultSignInControllerFactory implements SceneControllerFactory {
     @Override
     public Object createDefault() {
         MainMenuPageState mainMenuPageState = DefaultMainMenuPageState.getInstance();
-        SignInApi signInApi = new DefaultSignInApi();
         TextStyler textStyler = DefaultTextStyler.getInstance();
 
-        return new SignInController(mainMenuPageState, signInApi, textStyler);
+        SignInApi signInApi = new DefaultSignInApi();
+        ObjectMapper objectMapper = DefaultObjectMapperProvider.getInstance().getObjectMapper();
+        
+        SignInModel signInModel = new DefaultSignInModel(signInApi, objectMapper);
+
+        return new SignInController(mainMenuPageState, textStyler, signInModel);
     }
     
 }
