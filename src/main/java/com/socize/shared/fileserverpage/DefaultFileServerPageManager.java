f227e039e42765904ec279c9b2d52f81fc82c55d
YT-07 <no-email>
2025-06-11 20:38:03 +0800

    Allow file server page manager to directly change the main menu page state to encapsulate logic

--- src/main/java/com/socize/shared/fileserverpage/DefaultFileServerPageManager.java
+++ src/main/java/com/socize/shared/fileserverpage/DefaultFileServerPageManager.java
@@ -2,18 +2,24 @@ package com.socize.shared.fileserverpage;
 
 import com.socize.shared.fileserverpage.fileserverpagestatus.DefaultFileServerPageStatus;
 import com.socize.shared.fileserverpage.fileserverpagestatus.FileServerPageStatus;
+import com.socize.shared.mainmenupagestate.DefaultMainMenuPageState;
+import com.socize.shared.mainmenupagestate.MainMenuPageState;
 
 public class DefaultFileServerPageManager implements FileServerPageManager {
     private FileServerPageStatus status;
+
+    private final MainMenuPageState mainMenuPageState;
     private final Object loginStatusLock;
 
-    private DefaultFileServerPageManager() {
+    private DefaultFileServerPageManager(MainMenuPageState mainMenuPageState) {
         status = DefaultFileServerPageStatus.AT_HOME; // default page
+
+        this.mainMenuPageState = mainMenuPageState;
         loginStatusLock = new Object();
     }
 
     private static class SingletonInstanceHolder {
-        private static final DefaultFileServerPageManager INSTANCE = new DefaultFileServerPageManager();
+        private static final DefaultFileServerPageManager INSTANCE = new DefaultFileServerPageManager(DefaultMainMenuPageState.getInstance());
     }
 
     public static DefaultFileServerPageManager getInstance() {
@@ -25,6 +31,7 @@ public class DefaultFileServerPageManager implements FileServerPageManager {
         
         synchronized(loginStatusLock) {
             this.status = status;
+            mainMenuPageState.setPage(status.getFileServerPage());
         }
 
     }
