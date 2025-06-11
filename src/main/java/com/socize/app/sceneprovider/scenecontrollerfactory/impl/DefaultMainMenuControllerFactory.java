7571f3622fff05647aa7e3d5b4dc9a2186f7e1c2
YT-07 <no-email>
2025-06-11 21:29:24 +0800

    Changed logic for displaying page when clicking file server button

--- src/main/java/com/socize/app/sceneprovider/scenecontrollerfactory/impl/DefaultMainMenuControllerFactory.java
+++ src/main/java/com/socize/app/sceneprovider/scenecontrollerfactory/impl/DefaultMainMenuControllerFactory.java
@@ -4,6 +4,8 @@ import com.socize.app.sceneprovider.DefaultSceneProvider;
 import com.socize.app.sceneprovider.SceneProvider;
 import com.socize.app.sceneprovider.scenecontrollerfactory.spi.SceneControllerFactory;
 import com.socize.pages.PageController;
+import com.socize.pages.fileserver.shared.fileserverpage.DefaultFileServerPageManager;
+import com.socize.pages.fileserver.shared.fileserverpage.FileServerPageManager;
 import com.socize.pages.mainmenu.MainMenuController;
 import com.socize.pages.mainmenu.shared.mainmenupagestate.DefaultMainMenuPageState;
 import com.socize.pages.mainmenu.shared.mainmenupagestate.MainMenuPageState;
@@ -14,8 +16,9 @@ public class DefaultMainMenuControllerFactory implements SceneControllerFactory 
     public PageController createDefault() {
         MainMenuPageState pageState = DefaultMainMenuPageState.getInstance();
         SceneProvider provider = DefaultSceneProvider.getInstance();
+        FileServerPageManager fileServerPageManager = DefaultFileServerPageManager.getInstance();
 
-        return new MainMenuController(pageState, provider);
+        return new MainMenuController(pageState, provider, fileServerPageManager);
     }
     
 }
