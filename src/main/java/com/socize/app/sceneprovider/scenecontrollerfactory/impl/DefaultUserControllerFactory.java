e6e32244577b1a9c3fe095b97a4938b1c70ffb5a
YT-07 <no-email>
2025-06-12 19:06:36 +0800

    Fixed dependency for factory class

--- src/main/java/com/socize/app/sceneprovider/scenecontrollerfactory/impl/DefaultUserControllerFactory.java
+++ src/main/java/com/socize/app/sceneprovider/scenecontrollerfactory/impl/DefaultUserControllerFactory.java
@@ -1,5 +1,6 @@
 package com.socize.app.sceneprovider.scenecontrollerfactory.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.socize.api.getdownloadablefiles.DefaultGetDownloadableFilesApi;
 import com.socize.api.getdownloadablefiles.GetDownloadableFilesApi;
 import com.socize.app.sceneprovider.scenecontrollerfactory.spi.SceneControllerFactory;
@@ -11,6 +12,7 @@ import com.socize.pages.fileserver.user.model.DefaultUserModel;
 import com.socize.pages.fileserver.user.model.UserModel;
 import com.socize.pages.fileserver.utilities.logoutservice.DefaultLogoutService;
 import com.socize.pages.fileserver.utilities.logoutservice.LogoutService;
+import com.socize.utilities.objectmapper.DefaultObjectMapperProvider;
 
 public class DefaultUserControllerFactory implements SceneControllerFactory {
 
@@ -20,8 +22,9 @@ public class DefaultUserControllerFactory implements SceneControllerFactory {
 
         LogoutService logoutService = DefaultLogoutService.getInstance();
         GetDownloadableFilesApi getDownloadableFilesApi = new DefaultGetDownloadableFilesApi();
+        ObjectMapper objectMapper = DefaultObjectMapperProvider.getInstance().getObjectMapper();
 
-        UserModel userModel = new DefaultUserModel(logoutService, getDownloadableFilesApi);
+        UserModel userModel = new DefaultUserModel(logoutService, getDownloadableFilesApi, objectMapper);
 
         return new UserController(sessionManager, userModel);
     }
