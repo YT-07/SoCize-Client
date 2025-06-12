91deeb08769237bca4cb6994d20754b8baf0b897
YT-07 <no-email>
2025-06-12 16:29:48 +0800

    Added interface and implementation for the api to retrieve all downloadable files

---
+++ src/main/java/com/socize/api/getdownloadablefiles/DefaultGetDownloadableFilesApi.java
@@ -0,0 +1,16 @@
+package com.socize.api.getdownloadablefiles;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+
+import com.socize.api.getdownloadablefiles.dto.GetDownloadableFilesRequest;
+
+public class DefaultGetDownloadableFilesApi implements GetDownloadableFilesApi {
+
+    @Override
+    public CloseableHttpResponse getDownloadableFiles(GetDownloadableFilesRequest getDownloadableFilesRequest) {
+        // TODO
+
+        return null;
+    }
+    
+}
