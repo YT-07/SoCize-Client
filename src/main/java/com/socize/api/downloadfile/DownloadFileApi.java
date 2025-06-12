03422117c7033a6c0a700e5b9d3a93b8338d6262
YT-07 <no-email>
2025-06-12 21:10:16 +0800

    Added interface and implementation for file download api

---
+++ src/main/java/com/socize/api/downloadfile/DownloadFileApi.java
@@ -0,0 +1,16 @@
+package com.socize.api.downloadfile;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+
+import com.socize.api.downloadfile.dto.DownloadFileRequest;
+
+public interface DownloadFileApi {
+    
+    /**
+     * Attempts to make an api call to request for a file to be downloaded.
+     * 
+     * @param request the api request
+     * @return the api response
+     */
+    CloseableHttpResponse downloadFile(DownloadFileRequest request);
+}
