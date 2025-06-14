270972f8b52c64dfdb0041ed4b756943eb1ff445
YT-07 <no-email>
2025-06-14 15:04:50 +0800

    Added controller for user management page

---
+++ src/main/java/com/socize/pages/fileserver/admin/usermanagement/UserManagementController.java
@@ -0,0 +1,24 @@
+package com.socize.pages.fileserver.admin.usermanagement;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.socize.pages.PageController;
+
+import javafx.fxml.Initializable;
+
+public class UserManagementController extends PageController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+
+    @Override
+    public void onEnter() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onEnter'");
+    }
+    
+}
