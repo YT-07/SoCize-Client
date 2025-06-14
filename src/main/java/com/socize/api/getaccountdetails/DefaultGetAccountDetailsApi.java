c6a16d2375fb5ce78bfa0bff13b2d568f8f72496
YT-07 <no-email>
2025-06-14 19:57:41 +0800

    Added interface and implementation for api to get user account details

---
+++ src/main/java/com/socize/api/getaccountdetails/DefaultGetAccountDetailsApi.java
@@ -0,0 +1,16 @@
+package com.socize.api.getaccountdetails;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+
+import com.socize.api.getaccountdetails.dto.GetAccountDetailsRequest;
+
+public class DefaultGetAccountDetailsApi implements GetAccountDetailsApi {
+
+    @Override
+    public CloseableHttpResponse getAccountDetails(GetAccountDetailsRequest request) {
+        // TODO
+
+        return null;
+    }
+    
+}
---
+++ src/main/java/com/socize/api/getaccountdetails/GetAccountDetailsApi.java
@@ -0,0 +1,16 @@
+package com.socize.api.getaccountdetails;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+
+import com.socize.api.getaccountdetails.dto.GetAccountDetailsRequest;
+
+public interface GetAccountDetailsApi {
+
+    /**
+     * Attempts to make an api call to get the account details of a specified user.
+     * 
+     * @param request the api request
+     * @return the api response
+     */
+    CloseableHttpResponse getAccountDetails(GetAccountDetailsRequest request);
+}
