992822883cfa05f2a919ab882cd25bff8ca95fb2
YT-07 <no-email>
2025-06-14 15:04:32 +0800

    Added controller for server healthcheck page

---
+++ src/main/java/com/socize/pages/fileserver/admin/serverhealthcheck/ServerHealthcheckController.java
@@ -0,0 +1,24 @@
+package com.socize.pages.fileserver.admin.serverhealthcheck;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.socize.pages.PageController;
+
+import javafx.fxml.Initializable;
+
+public class ServerHealthcheckController extends PageController implements Initializable {
+
+    @Override
+    public void onEnter() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onEnter'");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+    
+}
