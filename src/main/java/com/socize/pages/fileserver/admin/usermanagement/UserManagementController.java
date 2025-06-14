8d57a3b00c0437813219738d0a132a3e0ed8234e
YT-07 <no-email>
2025-06-14 19:49:42 +0800

    Added fxml fields to controller

--- src/main/java/com/socize/pages/fileserver/admin/usermanagement/UserManagementController.java
+++ src/main/java/com/socize/pages/fileserver/admin/usermanagement/UserManagementController.java
@@ -5,9 +5,40 @@ import java.util.ResourceBundle;
 
 import com.socize.pages.PageController;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class UserManagementController extends PageController implements Initializable {
+
+    @FXML
+    private Button deleteUserButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Button viewUserDetailButton;
+
+    @FXML
+    private Label accountEmailField;
+
+    @FXML
+    private Label accountPhoneNumberField;
+
+    @FXML
+    private Label accountUsernameField;
+
+    @FXML
+    private Label feedbackMessageField;
+
+    @FXML
+    private Label usernameDisplayField;
+
+    @FXML
+    private ListView<String> userListView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
