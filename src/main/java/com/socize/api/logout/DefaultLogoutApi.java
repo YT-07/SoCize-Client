01068bdc3c9bf7b9e8546911f758591ec12188bc
YT-07 <no-email>
2025-06-25 20:29:36 +0800

    Modified interface to throw exception on error and implemented logout api call

--- src/main/java/com/socize/api/logout/DefaultLogoutApi.java
+++ src/main/java/com/socize/api/logout/DefaultLogoutApi.java
@@ -1,16 +1,35 @@
 package com.socize.api.logout;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.socize.api.logout.dto.LogoutRequest;
 
 public class DefaultLogoutApi implements LogoutApi {
+    private final ObjectMapper objectMapper;
+    private final CloseableHttpClient client;
 
+    public DefaultLogoutApi(ObjectMapper objectMapper, CloseableHttpClient client) {
+        this.objectMapper = objectMapper;
+        this.client = client;
+    }
+    
     @Override
-    public CloseableHttpResponse logout(LogoutRequest logoutRequest) {
-        // TODO
+    public CloseableHttpResponse logout(LogoutRequest logoutRequest) throws Exception {
+        HttpPost request = new HttpPost("localhost/SoCize-Server/API/logout.php");
 
-        return null;
+        String json = objectMapper.writeValueAsString(request);
+
+        StringEntity entity = new StringEntity(json, StandardCharsets.UTF_8);
+        entity.setContentType("application/json");
+        request.setEntity(entity);
+
+        return client.execute(request);
     }
     
 }
--- src/main/java/com/socize/api/logout/LogoutApi.java
+++ src/main/java/com/socize/api/logout/LogoutApi.java
@@ -11,6 +11,7 @@ public interface LogoutApi {
      * 
      * @param logoutRequest the logout request containing necessary data to logout
      * @return the api response
+     * @throws Exception if any exception occur
      */
-    CloseableHttpResponse logout(LogoutRequest logoutRequest);
+    CloseableHttpResponse logout(LogoutRequest logoutRequest) throws Exception;
 }
