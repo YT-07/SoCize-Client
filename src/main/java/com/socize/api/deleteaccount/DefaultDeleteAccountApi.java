d833f74b136797dfd68a18a830b9cd205534c0f5
YT-07 <no-email>
2025-06-25 21:20:49 +0800

    Implemented api call to delete user account

--- src/main/java/com/socize/api/deleteaccount/DefaultDeleteAccountApi.java
+++ src/main/java/com/socize/api/deleteaccount/DefaultDeleteAccountApi.java
@@ -1,16 +1,35 @@
 package com.socize.api.deleteaccount;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.socize.api.deleteaccount.dto.DeleteAccountRequest;
 
 public class DefaultDeleteAccountApi implements DeleteAccountApi {
+    private final ObjectMapper objectMapper;
+    private final CloseableHttpClient client;
+
+    public DefaultDeleteAccountApi(ObjectMapper objectMapper, CloseableHttpClient client) {
+        this.objectMapper = objectMapper;
+        this.client = client;
+    }
 
     @Override
-    public CloseableHttpResponse deleteAccount(DeleteAccountRequest request) {
-        // TODO
+    public CloseableHttpResponse deleteAccount(DeleteAccountRequest deleteRequest) throws Exception {
+        HttpPost request = new HttpPost("localhost/SoCize-Server/API/DeleteUser.php");
 
-        return null;
+        String json = objectMapper.writeValueAsString(deleteRequest);
+
+        StringEntity entity = new StringEntity(json, StandardCharsets.UTF_8);
+        entity.setContentType("application/json");
+        request.setEntity(entity);
+
+        return client.execute(request);
     }
     
 }
--- src/main/java/com/socize/api/deleteaccount/DeleteAccountApi.java
+++ src/main/java/com/socize/api/deleteaccount/DeleteAccountApi.java
@@ -11,6 +11,7 @@ public interface DeleteAccountApi {
      * 
      * @param request the api request
      * @return the api response
+     * @throws Exception if any exception occurs
      */
-    CloseableHttpResponse deleteAccount(DeleteAccountRequest request);
+    CloseableHttpResponse deleteAccount(DeleteAccountRequest request) throws Exception;
 }
