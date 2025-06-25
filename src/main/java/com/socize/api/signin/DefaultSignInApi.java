93d6d1bed28a0826f23ca481729942a3715f8fb6
YT-07 <no-email>
2025-06-25 20:17:59 +0800

    Modified interface to throw exception on error and implemented sign in api call

--- src/main/java/com/socize/api/signin/DefaultSignInApi.java
+++ src/main/java/com/socize/api/signin/DefaultSignInApi.java
@@ -1,16 +1,35 @@
 package com.socize.api.signin;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.socize.api.signin.dto.SignInRequest;
 
 public class DefaultSignInApi implements SignInApi {
+    private final ObjectMapper objectMapper;
+    private final CloseableHttpClient client;
+
+    public DefaultSignInApi(ObjectMapper objectMapper, CloseableHttpClient client) {
+        this.objectMapper = objectMapper;
+        this.client = client;
+    }
 
     @Override
-    public CloseableHttpResponse signin(SignInRequest signInRequest) {
-        // TODO
+    public CloseableHttpResponse signin(SignInRequest signInRequest) throws Exception {
+        HttpPost request = new HttpPost("localhost/SoCize-Server/API/login.php");
 
-        return null;
+        String json = objectMapper.writeValueAsString(request);
+
+        StringEntity entity = new StringEntity(json, StandardCharsets.UTF_8);
+        entity.setContentType("application/json");
+        request.setEntity(entity);
+
+        return client.execute(request);
     }
     
 }
